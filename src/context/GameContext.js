45de31c9dbb710e3e3d0cbbbfebc055f5a4a4dde
bkfan1 <blossomsc15@gmail.com>
2022-05-10 19:03:51 -0400

    Refactored context, added new logic and fixed bugs in some previous functions

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,131 +1,136 @@
-import { createContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { createContext } from "react";
 import { gameLetters } from "../utils/letters";
 import { gameWords } from "../utils/words";
 
 export const GameContext = createContext();
 
-export function GameProvider({ children }) {
+export const GameProvider = ({ children }) => {
   const [matchLetters, setMatchLetters] = useState([]);
   const [matchWords, setMatchWords] = useState([]);
 
-  const [guessedWords, setGuessedWords] = useState([]);
-
   const [selectedLetters, setSelectedLetters] = useState([]);
 
-  const [clueWord, setClueWord] = useState(null);
-
-  const [clueCounter, setClueCounter] = useState(3);
+  const [guessedWords, setGuessedWords] = useState([]);
 
   const [percentCompleted, setPercentCompleted] = useState(0);
 
+  const [clueCounter, setClueCounter] = useState(3);
+  const [clueWord, setClueWord] = useState(null);
+
+  const [guessingAttempts, setGuessingAttempts] = useState(0);
   const [error, setError] = useState(null);
 
   const setMatch = () => {
-    setPercentCompleted(0);
-    setSelectedLetters([]);
     setMatchLetters([]);
     setMatchWords([]);
     setGuessedWords([]);
+    setSelectedLetters([]);
+    setGuessingAttempts(0);
+    setClueCounter(3);
+    setClueWord(null);
+    setError(null);
+    setPercentCompleted(0);
 
     const randInt = Math.floor(Math.random() * 5);
-    setMatchLetters(gameLetters[randInt]);
+    let objectLetters = [];
+    gameLetters[randInt].forEach((letter, index) => {
+      objectLetters.push({ id: index, value: letter });
+    });
+
+    setMatchLetters(objectLetters);
 
     let upperCasedWords = [];
-    gameWords[randInt].forEach((word) =>
-      upperCasedWords.push(word.toUpperCase())
-    );
+    gameWords[randInt].forEach((word) => {
+      upperCasedWords.push(word.toUpperCase());
+    });
 
     setMatchWords(upperCasedWords);
+  };
+
+  const resetError = () => {
+    setTimeout(() => {
+      setError(null);
+    }, 2000);
   };
 
   useEffect(() => {
     setMatch();
   }, []);
 
+  useEffect(() => {}, [error]);
+
   const handleClickLetterBtn = (e) => {
-    const { target } = e;
-    const { value } = target;
-
-    if (selectedLetters.includes(value)) {
-      const newSelectedLetters = [...selectedLetters];
-
-      const index = selectedLetters.findIndex((letter) => letter === value);
-
-      newSelectedLetters.splice(index, 1);
-
-      setSelectedLetters(newSelectedLetters);
+    const id = Number(e.target.value);
+    let letter = matchLetters.find((letter) => letter.id === id);
+    console.log(letter);
+    if (!selectedLetters.includes(letter)) {
+      setSelectedLetters([...selectedLetters, letter]);
     } else {
-      setSelectedLetters([...selectedLetters, value]);
+      const index = selectedLetters.findIndex((letter) => letter.id === id);
+      const updatedSelectedLetters = [...selectedLetters];
+      updatedSelectedLetters.splice(index, 1);
+      setSelectedLetters(updatedSelectedLetters);
     }
   };
-
-  const handleClickClueBtn = () => {
-
-    if(clueCounter > 0){
-
-      let randInt = Math.floor(Math.random()*30);
-
-      if(guessedWords.includes(matchWords[randInt])){
-        randInt -= 1;
-        setClueWord(matchWords[randInt]);
-      }
-      else{setClueWord(matchWords[randInt])}
-
-      setClueCounter(clueCounter-1);
-    }
-
-  };
-
-  const handleSubmitFormingWord = () => {
-    const wordAttempt = [...selectedLetters].join("");
-
-    console.log(wordAttempt);
-
-    if (matchWords.includes(wordAttempt)) {
-      if (guessedWords.includes(wordAttempt)) {
+  const handleClickMergeLetters = () => {
+    let lettersValues = [];
+    selectedLetters.forEach((letter) => lettersValues.push(letter.value));
+    console.log(lettersValues);
+    let formedWord = lettersValues.join("");
+    console.log(formedWord);
+    if (matchWords.includes(formedWord)) {
+      if (guessedWords.includes(formedWord)) {
         setSelectedLetters([]);
-        setError("Word already guessed");
-        setTimeout(() => {
-          setError(null);
-        }, 2000);
+        setError("Word already guessed!");
+        resetError();
+        setGuessingAttempts(guessingAttempts + 1);
       } else {
-        setError("");
-        setGuessedWords([...guessedWords, wordAttempt]);
+        setError(null);
         setSelectedLetters([]);
+        setGuessedWords([...guessedWords, formedWord]);
         setPercentCompleted(percentCompleted + 5);
       }
     } else {
       setSelectedLetters([]);
-      setError("Invalid word");
-      setTimeout(() => {
-        setError(null);
-      }, 2000);
+      setError("Incorrect word!");
+      resetError();
+      setGuessingAttempts(guessingAttempts + 1);
+    }
+  };
+
+  const handleClickClueBtn = () => {
+    if (clueCounter > 0) {
+      let randInt = Math.floor(Math.random() * matchWords.length);
+
+      if (guessedWords.includes(matchWords[randInt])) {
+        randInt -= 1;
+        setClueWord(matchWords[randInt]);
+      } else {
+        setClueWord(matchWords[randInt]);
+      }
+
+      setClueCounter(clueCounter - 1);
     }
   };
 
   return (
     <GameContext.Provider
       value={{
-        matchLetters,
-        setMatchLetters,
-        matchWords,
-        setMatchWords,
-        guessedWords,
-        setGuessedWords,
-        selectedLetters,
-        setSelectedLetters,
-        handleClickLetterBtn,
-        handleSubmitFormingWord,
-        error,
-        setError,
-        percentCompleted,
         setMatch,
-        handleClickClueBtn,
+        matchLetters,
+        selectedLetters,
+        guessedWords,
         clueWord,
-        clueCounter,
+        guessingAttempts,
+        percentCompleted,
+        handleClickLetterBtn,
+        handleClickMergeLetters,
+        handleClickClueBtn,
+        error,
       }}
     >
       {children}
     </GameContext.Provider>
   );
-}
+};
